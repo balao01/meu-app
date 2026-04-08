b356509e8e914f2d3a5850f3375c277a198e157d
balao01 <ls570487@gmail.com>
2026-04-08 00:55:05 -0300

    Add App component with red background and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,9 @@
-
+export default function App() {
+  return (
+    <div style={{ background: "red", height: "100vh" }}>
+      <h1 style={{ color: "white" }}>
+        TESTE FINAL 123456
+      </h1>
+    </div>
+  );
+}
